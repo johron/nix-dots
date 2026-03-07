88da3d1f7a6985cca60e950deb82201226345e14
Johan Rong <johanhrong@icloud.com>
2026-03-07 22:07:49 +0100

    changes

--- hosts/desktop-nixos-workstation/networking.nix
+++ hosts/desktop-nixos-workstation/networking.nix
@@ -4,6 +4,13 @@ let
 in
 
 {
+  environment.systemPackages = with pkgs; [
+    protonvpn-gui
+    wireguard-tools
+  ];
+
   networking.hostName = hosts.nixstation.hostname;
   networking.networkmanager.enable = true;
+
+  networking.firewall.checkReversePath = false;
 }
--- nixos/features/system/gaming.nix
+++ nixos/features/system/gaming.nix
@@ -3,6 +3,7 @@
   environment.systemPackages = with pkgs; [
     lutris
     prismlauncher
+    nvidia-vaapi-driver
   ];
 
   services.flatpak = {
