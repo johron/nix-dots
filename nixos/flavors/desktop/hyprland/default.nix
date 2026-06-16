091e84bc389debc7550558395bf85ac8018afcf1
Johan Rong <johanhrong@icloud.com>
2026-06-16 17:46:06 +0200

    ok

--- nixos/flavors/desktop/hyprland/default.nix
+++ nixos/flavors/desktop/hyprland/default.nix
@@ -66,4 +66,13 @@
 
   services.gnome.gnome-keyring.enable = true;
   security.pam.services.login.enableGnomeKeyring = true;
+  security.pam.services.hyprland.enableGnomeKeyring = true;
+
+  systemd.user.services.gnome-keyring = {
+    wantedBy = [ "default.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.gnome-keyring}/bin/gnome-keyring-daemon --start --foreground --components=pkcs11,secrets,ssh";
+      Restart = "on-abort";
+    };
+  };
 }
