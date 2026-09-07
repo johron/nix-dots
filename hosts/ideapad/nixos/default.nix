ec48903627b305f80db7fe71ae096370e8a21b6c
Johan Rong <johanhrong@icloud.com>
2026-09-07 16:14:30 +0200

    try tuigreet

--- hosts/ideapad/nixos/default.nix
+++ hosts/ideapad/nixos/default.nix
@@ -21,8 +21,8 @@ in
     ../../../nixos/modules/hardware/biometric/elan-fingerprint.nix
 
     ../../../nixos/modules/desktop/hyprland
-    ../../../nixos/modules/greeter/dank-greeter/hyprland.nix
-
+    #../../../nixos/modules/greeter/dank-greeter/hyprland.nix
+    ../../../nixos/modules/greeter/tuigreet
   ];
 
   boot.kernelParams = [
--- hosts/nixstation/nixos/default.nix
+++ hosts/nixstation/nixos/default.nix
@@ -21,7 +21,8 @@ in
     ../../../nixos/modules/hardware/bluetooth
 
     ../../../nixos/modules/desktop/hyprland
-    ../../../nixos/modules/greeter/dank-greeter/hyprland.nix
+    #../../../nixos/modules/greeter/dank-greeter/hyprland.nix
+    ../../../nixos/modules/greeter/tuigreet
   ];
   
   boot.kernelParams = [
