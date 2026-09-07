766d5dc8d50095eecd7d1a6e796a98a80ee7cbff
Johan Rong <johanhrong@icloud.com>
2026-09-07 15:55:10 +0200

    try fix dank greeter

--- flake.nix
+++ flake.nix
@@ -12,10 +12,6 @@
       url = "github:AvengeMedia/DankMaterialShell/stable";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    dank-greeter = {
-      url = "github:AvengeMedia/dank-greeter";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs =
