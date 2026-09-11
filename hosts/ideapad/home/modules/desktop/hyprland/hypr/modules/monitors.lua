3cbc75aad326e1b9140b6dc87a713e11f869eb09
Johan Rong <johanhrong@icloud.com>
2026-09-11 21:21:59 +0200

    ok

--- hosts/ideapad/home/modules/desktop/hyprland/hypr/modules/monitors.lua
+++ hosts/ideapad/home/modules/desktop/hyprland/hypr/modules/monitors.lua
@@ -1,1 +1,3 @@
+hl.monitor({ output = "HDMI-A-1", mode = "preferred", position = "auto", scale = 1 })
+hl.monitor({ output = "eDP-1", mode = "preferred", position = "auto", scale = 1 })
 hl.monitor({ output = "", mode = "preferred", position = "auto", scale = 1, mirror = "eDP-1" })
