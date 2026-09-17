9323ac7a5b325ccec4184253d69b670c7b14888d
Johan Rong <johanhrong@icloud.com>
2026-09-17 22:52:50 +0200

    do nothing basically

--- home/modules/desktop/hyprland/default.nix
+++ home/modules/desktop/hyprland/default.nix
@@ -4,56 +4,54 @@ let
   hostname = host.hostname;
 in 
 {
-  config = {
-    home.file.".config/hypr/hyprland.lua".source = ./lua/hyprland.lua;
-    home.file.".config/hypr/modules/autostart.lua".source = ./lua/modules/autostart.lua; 
-    home.file.".config/hypr/modules/environment.lua".source = ./lua/modules/environment.lua; 
-    home.file.".config/hypr/modules/look_and_feel.lua".source = ./lua/modules/look_and_feel.lua; 
-    home.file.".config/hypr/modules/misc.lua".source = ./lua/modules/misc.lua; 
-    home.file.".config/hypr/modules/input.lua".source = ./lua/modules/input.lua; 
-    home.file.".config/hypr/modules/keybindings.lua".source = ./lua/modules/keybindings.lua; 
-    home.file.".config/hypr/modules/windows_and_workspaces.lua".source = ./lua/modules/windows_and_workspaces.lua; 
+  home.file.".config/hypr/hyprland.lua".source = ./lua/hyprland.lua;
+  home.file.".config/hypr/modules/autostart.lua".source = ./lua/modules/autostart.lua; 
+  home.file.".config/hypr/modules/environment.lua".source = ./lua/modules/environment.lua; 
+  home.file.".config/hypr/modules/look_and_feel.lua".source = ./lua/modules/look_and_feel.lua; 
+  home.file.".config/hypr/modules/misc.lua".source = ./lua/modules/misc.lua; 
+  home.file.".config/hypr/modules/input.lua".source = ./lua/modules/input.lua; 
+  home.file.".config/hypr/modules/keybindings.lua".source = ./lua/modules/keybindings.lua; 
+  home.file.".config/hypr/modules/windows_and_workspaces.lua".source = ./lua/modules/windows_and_workspaces.lua; 
 
-    xdg = {
-      userDirs = {
-        enable = true;
-        createDirectories = true;
-        
-        documents = "$HOME/Documents";
-        download = "$HOME/Downloads";
-        desktop = "$HOME/Desktop";
-        music = "$HOME/Music";
-        pictures = "$HOME/Pictures";
-        videos = "$HOME/Videos";
-        projects = "$HOME/Projects";
-      };
+  xdg = {
+    userDirs = {
+      enable = true;
+      createDirectories = true;
+      
+      documents = "$HOME/Documents";
+      download = "$HOME/Downloads";
+      desktop = "$HOME/Desktop";
+      music = "$HOME/Music";
+      pictures = "$HOME/Pictures";
+      videos = "$HOME/Videos";
+      projects = "$HOME/Projects";
+    };
 
-      mimeApps = {
-        enable = true;
+    mimeApps = {
+      enable = true;
 
-        defaultApplications = {
-          "inode/directory" = [ "org.kde.dolphin.desktop" ];
+      defaultApplications = {
+        "inode/directory" = [ "org.kde.dolphin.desktop" ];
 
-          "x-scheme-handler/http" = "firefox.desktop";
-          "x-scheme-handler/https" = "firefox.desktop";
-          "text/plain" = "code.desktop";
-          "text/csv" = "code.desktop";
-          "application/pdf" = "firefox.desktop";
-          "image/jpeg" = "org.nomacs.ImageLounge.desktop";
-          "image/png" = "org.nomacs.ImageLounge.desktop";
-          "image/gif" = "org.nomacs.ImageLounge.desktop";
-          "image/webp" = "org.nomacs.ImageLounge.desktop";
-          "image/bmp" = "org.nomacs.ImageLounge.desktop";
-          "image/tiff" = "org.nomacs.ImageLounge.desktop";
-          "image/svg+xml" = "org.nomacs.ImageLounge.desktop";
-          "image/x-icon" = "org.nomacs.ImageLounge.desktop";
-          "audio/mpeg" = "vlc.desktop";
-          "video/mp4" = "vlc.desktop";
-          "video/quicktime" = "vlc.desktop";
-          "video/vnd.avi" = "vlc.desktop";
-          "application/x-compressed-tar" = "org.kde.ark.desktop";
-          "x-scheme-handler/unknown" = "dms-open.desktop";
-        };
+        "x-scheme-handler/http" = "firefox.desktop";
+        "x-scheme-handler/https" = "firefox.desktop";
+        "text/plain" = "code.desktop";
+        "text/csv" = "code.desktop";
+        "application/pdf" = "firefox.desktop";
+        "image/jpeg" = "org.nomacs.ImageLounge.desktop";
+        "image/png" = "org.nomacs.ImageLounge.desktop";
+        "image/gif" = "org.nomacs.ImageLounge.desktop";
+        "image/webp" = "org.nomacs.ImageLounge.desktop";
+        "image/bmp" = "org.nomacs.ImageLounge.desktop";
+        "image/tiff" = "org.nomacs.ImageLounge.desktop";
+        "image/svg+xml" = "org.nomacs.ImageLounge.desktop";
+        "image/x-icon" = "org.nomacs.ImageLounge.desktop";
+        "audio/mpeg" = "vlc.desktop";
+        "video/mp4" = "vlc.desktop";
+        "video/quicktime" = "vlc.desktop";
+        "video/vnd.avi" = "vlc.desktop";
+        "application/x-compressed-tar" = "org.kde.ark.desktop";
+        "x-scheme-handler/unknown" = "dms-open.desktop";
       };
     };
   };
