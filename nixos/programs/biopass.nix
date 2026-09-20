444e931abc5cee77a00645d7f97f0846392a1a20
Johan Rong <johanhrong@icloud.com>
2026-09-20 12:46:35 +0200

    biopass

---
+++ nixos/programs/biopass.nix
@@ -0,0 +1,23 @@
+{ lib, stdenv, fetchFromGitHub, cmake, pkg-config, openssl, onnxruntime, glib, gtk3 }:
+
+stdenv.mkDerivation rec {
+  pname = "biopass";
+  version = "1.4.1";
+
+  src = fetchFromGitHub {
+    owner = "ticklabvn";
+    repo = "biopass";
+    rev = "v${version}";
+    hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+  };
+
+  nativeBuildInputs = [ cmake pkg-config ];
+  buildInputs = [ openssl onnxruntime glib gtk3 ];
+
+  meta = with lib; {
+    description = "An alternative to Windows Hello/Howdy";
+    homepage = "https://github.com/TickLabVN/biopass";
+    license = licenses.mit;
+    platforms = platforms.linux;
+  };
+}
