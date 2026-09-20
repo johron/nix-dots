f047b7b9a49987a9267cd5898c544d6f26761504
Johan Rong <johanhrong@icloud.com>
2026-09-20 17:18:09 +0200

    fingeprrint

--- hosts/ono/nixos/default.nix
+++ hosts/ono/nixos/default.nix
@@ -19,7 +19,7 @@ in
     ../../../nixos/modules/hardware/networking
     ../../../nixos/modules/hardware/sound
     ../../../nixos/modules/hardware/bluetooth
-    # ../../../nixos/modules/hardware/biometric/fingerprint
+    ../../../nixos/modules/hardware/biometric/fingerprint
     ../../../nixos/modules/hardware/battery
 
     ../../../nixos/modules/desktop/hyprland
